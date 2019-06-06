3298b937283e2a18e565b184713a9c8ad515ad9b
Vianney Rslt <vianney.rslt@gmail.com>
2019-06-06 15:31:07 +0200

    création page profil twig

---
+++ src/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/profil/{id}", name="profil", methods={"GET"})
+     */
+    public function profil(User $user): Response
+    {
+        return $this->render('user/profil.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
