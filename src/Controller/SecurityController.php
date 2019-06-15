af77e1e56a637b4f7d203396499bbaff85932484
Vincent Sureau <vincentsureau5@gmail.com>
2019-06-15 17:24:17 +0200

    retiré consoller + template inutile + créer template pour mot de passe oublié + créer route pour gestion des mots de passe oubliés

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\PasswordType;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -23,6 +28,54 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
             'current' => 'login'
+            ]);
+    }
+
+    // Génère un nouveau mot de passe et envoi un mail à l'utilisateur
+
+    /**
+     * @Route("/mot-de-passe-oublie", name="app_forgotten_pasword")
+     */
+    public function newPassword(Request $request, UserPasswordEncoderInterface $encoder, UserRepository $repo): Response
+    {
+        $form = $this->createForm(PasswordType::class);
+        $data = $request->request->all();
+        $form->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {        
+            $email = $form->get('email')->getData();
+            $user = $repo->findOneByEmail($email);
+
+            if($user) {
+                // encode the plain password
+                $user->setPassword(
+                    $encoder->encodePassword(
+                        $user,
+                        'admin'
+                    )
+                );
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Un nouveau mot de passe vous a été envoyé par mail'
+                );
+                // do anything else you need here, like send an email
+
+                return $this->redirectToRoute('app_login');
+            }
+            
+            $this->addFlash(
+                'danger',
+                'L\'email saisi est incorrect'
+            );
+
+        }
+        return $this->render('security/password.html.twig', [
+            'current' => 'password'
             ]);
     }
 }
---
+++ src/Form/PasswordType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class PasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci d\'entrer un mot de passe',
+                    ]),
+                    new Email([
+                        'message' => 'Merci d\'entrer un email valide',
+                    ])
+            ]])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'csrf_protection' => false,
+        ]);
+    }
+}
