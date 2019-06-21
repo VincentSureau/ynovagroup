cd9cb4a452d6a54cee00a083cbed07308f8c29c2
Sara Cammi <cammisara@outlook.fr>
2019-06-21 10:18:32 +0200

    Correction bug links + Création fichier js

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+console.log("app.js is loaded");
+
+// MODAL DISPLAY ON PAGE LOAD
+    // window.addEventListener('DOMContentLoaded', function() {
+    //     let popup = document.getElementById('popup');
+    //     popup.style.display = "block";
+    // });
+    $(window).on('load',function(){
+        $('#myModal').modal('show');
+    });
+
+// SCROLL TOP BUTTON
+    window.onscroll = function() {scrollFunction()};
+
+     function topFunction() {
+        window.scrollTo(0,0);
+    };
+
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800) {
+        document.getElementById("myBtn").style.display = "block";
+        } else {
+        document.getElementById("myBtn").style.display = "none";
+        }
+    }
+
+   
+// PARALLAX IMAGE ON HOMEPAGE
+    function parallax() {
+        var $slider = document.getElementById("parallax");
+        var yPos = window.pageYOffset / $slider.dataset.speed;
+        yPos = -yPos;
+        var coords = '0% '+ yPos + 'px';
+        $slider.style.backgroundPosition = coords;
+    }
+    window.addEventListener("scroll", function(){
+        parallax();	
+    });
+    
+
