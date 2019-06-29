4bdff73a56e03fc3446d323fa28183c6fa4c6972
Vincent Sureau <vincentsureau5@gmail.com>
2019-06-29 11:49:34 +0200

    rendu page blog dynamique

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
