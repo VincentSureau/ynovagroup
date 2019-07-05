6eb07bdea21aa8d9a226963f68ac79c20e0aaac1
Vincent Sureau <vincentsureau5@gmail.com>
2019-07-05 16:07:29 +0200

    remplacé référence à blog par actualité

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/actualites", name="blog")
      */
     public function index(PostRepository $postRepository, PaginatorInterface $paginator, Request $request)
     {
