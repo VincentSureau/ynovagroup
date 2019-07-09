fdd7492356e4515ba064f39f6488f2ffa898d5c0
Vincent Sureau <vincentsureau5@gmail.com>
2019-07-09 12:45:33 +0200

    ajouté sécurité pour accès au donné sur pharmacies et gestionnaires

--- src/Controller/Api/CompanyController.php
+++ src/Controller/Api/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Repository\CompanyRepository;
+use Symfony\Component\Security\Core\Security;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,10 +14,17 @@ class CompanyController extends FOSRestController
     * @Rest\Get("/api/companies")
     * @Rest\View(serializerGroups={"company"})
     */
-    public function getListAction(CompanyRepository $repo) {
-        $data = $repo->findAll(
-            ['createdAt' => 'DESC']
-        );
+    public function getListAction(CompanyRepository $repo, Security $security) {
+        if ($security->isGranted('ROLE_ADMIN')) {
+            $data = $repo->findAll(
+                ['createdAt' => 'DESC']
+            );
+        } elseif ($security->isGranted('ROLE_BUSINESS')) {
+            $data = $repo->findBy(
+                ['commercial' => $this->getUser()],
+                ['name' => 'ASC', 'createdAt' => 'DESC']
+            );
+        }
 
         return $data;
     }
--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Security;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,8 +14,21 @@ class UserController extends FOSRestController
     * @Rest\Get("/api/users")
     * @Rest\View(serializerGroups={"user"})
     */
-    public function getListAction(UserRepository $repo) {
-        $data = $repo->findMembers();
+    public function getListAction(UserRepository $repo, Security $security) {
+        if ($security->isGranted('ROLE_ADMIN')) {
+            $data = $repo->findMembers();
+        } elseif ($security->isGranted('ROLE_BUSINESS')) {
+            $data = $repo->createQueryBuilder('u')
+                        ->innerJoin('u.company', 'c')
+                        ->where('u.roles LIKE :role')
+                        ->setParameter('role', '%"ROLE_MEMBER"%')
+                        ->andWhere('c.commercial = :commercial')
+                        ->setParameter('commercial', $this->getUser())
+                        ->orderBy('u.id', 'ASC')
+                        ->getQuery()
+                        ->getResult()
+                    ;
+        }
 
         return $data;
     }
